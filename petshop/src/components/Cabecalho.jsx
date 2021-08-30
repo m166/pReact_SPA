34d61c8530310e7bd429c6a4ccdd565322c487a2
m166 <cpxmat@gmail.com>
2021-08-30 12:10:10 -0300

    Router SPA

--- petshop/src/components/Cabecalho.jsx
+++ petshop/src/components/Cabecalho.jsx
@@ -3,50 +3,33 @@ import { Link } from 'react-router-dom'
 import imagem from '../assets/img/doguito.svg'
 import '../assets/css/componentes/cabecalho.css'
 
-const Cabecalho = () => {
-    return (
-        <header className="cabecalho container">
-            <div className="menu-hamburger">
-                <span className="menu-hamburger__icone">
-                </span>
-            </div>
-            <div className="cabecalho-container">
-                <Link to="/" className="flex flex--centro">
-                    <img className="cabecalho__logo" src={imagem} alt="Logo Doguito" />
-                    <h1 className="cabecalho__titulo">
-                        PetShop
-                    </h1>
-                </Link>
-            </div>
-            <nav className="menu-cabecalho">
-                <ul className="menu-itens">
-                    <li>
-                        <Link className="menu-item menu-item--entrar" to="#">
-                            Entrar
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className="menu-item" to="#">
-                            Produtos
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className="menu-item" to="/">
-                            Blog
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className="menu-item" to="/sobre">
-                            Sobre
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <div className="menu-cabecalho-background"></div>
-        </header>
-    )
+const Cabecalho = () => { 
+  return(
+    <header className="cabecalho container">
+      <div className="menu-hamburger">
+        <span className="menu-hamburger__icone">
+        </span>
+      </div>
+      <div className="cabecalho-container">
+        <Link to="/" className="flex flex--centro">
+          <img className="cabecalho__logo" src={imagem} alt="Logo Doguito"/>
+          <h1 className="cabecalho__titulo">PetShop</h1>
+        </Link>
+      </div>
 
+      <nav className="menu-cabecalho">
+        <ul className="menu-itens">
+          <li><Link to="#" className="menu-item menu-item--entrar">Entrar</Link></li>
+          <li><Link to="#" className="menu-item">Produtos</Link></li>
+          <li><Link to="/" className="menu-item">Blog</Link></li>
+          <li><Link to="/sobre" className="menu-item">Sobre</Link></li>
+        </ul>
+
+      </nav>
+      <div className="menu-cabecalho-background"></div>
+    </header>
+  )
 }
 
 
-export default Cabecalho;
+export default Cabecalho
